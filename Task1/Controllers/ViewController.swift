bd4b2a683a31665a5de9928c1d90a572b657154f
bushido117 <vadim.saiko53370@mail.ru>
2022-12-11 22:38:31 +0300

    FINISHED task

---
+++ Task1/Controllers/ViewController.swift
@@ -0,0 +1,133 @@
+//
+//  ViewController.swift
+//  Task1
+//
+//  Created by Вадим Сайко on 11.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private enum Languages: String, CaseIterable {
+            case english = "language_english"
+            case russian = "language_russian"
+            case belarusian = "language_belarusian"
+        
+            var languageSystemName: String {
+                switch self {
+                case .english:
+                    return "en"
+                case .russian:
+                    return "ru"
+                case .belarusian:
+                    return "be"
+            }
+        }
+    }
+    private let containerView = ContainerView()
+    private lazy var languagePicker = containerView.languagePicker
+    private lazy var keyWindow = UIApplication
+        .shared
+        .connectedScenes
+        .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
+        .first { $0.isKeyWindow }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        containerView.frame = UIScreen.main.bounds
+        view.addSubview(containerView)
+        languagePicker.delegate = self
+        languagePicker.dataSource = self
+        containerView.onDarkThemeButtonTapped = { [weak self] in
+            self?.darkThemeButtonTapped()
+        }
+        containerView.onLightThemeButtonTapped = { [weak self] in
+            self?.lightThemeButtonTapped()
+        }
+        containerView.onAutoThemeButtonTapped = { [weak self] in
+            self?.autoThemeButtonTapped()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        pickerViewSelectRow(for: languagePicker)
+    }
+    
+    func pickerViewSelectRow(for pickerView: UIPickerView) {
+        let row: Int = {
+            switch AppLanguage.currentLanguage() {
+            case Languages.allCases[0].languageSystemName:
+                return 0
+            case Languages.allCases[1].languageSystemName:
+                return 1
+            case Languages.allCases[2].languageSystemName:
+                return 2
+            default:
+                return 0
+            }
+        }()
+        pickerView.selectRow(row, inComponent: 0, animated: false)
+    }
+    
+    func refreshVC() {
+        let vc = ViewController()
+        keyWindow?.rootViewController = vc
+        keyWindow?.backgroundColor = .systemBackground
+    }
+    
+//MARK: Buttons Actions
+    func darkThemeButtonTapped() {
+        keyWindow?.overrideUserInterfaceStyle = .dark
+        UserDefaults.standard.set("dark", forKey: "UserInterfaceStyle")
+    }
+    
+    func lightThemeButtonTapped() {
+        keyWindow?.overrideUserInterfaceStyle = .light
+        UserDefaults.standard.set("light", forKey: "UserInterfaceStyle")
+    }
+
+    func autoThemeButtonTapped() {
+        keyWindow?.overrideUserInterfaceStyle = .unspecified
+    }
+}
+
+//MARK: - UIPickerViewDelegate
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        NSLocalizedString(Languages.allCases[row].rawValue, comment: "")
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch row {
+        case 0:
+            AppLanguage.setNewLanguage( Languages.allCases[row].languageSystemName)
+        case 1:
+            AppLanguage.setNewLanguage(Languages.allCases[row].languageSystemName)
+        case 2:
+            AppLanguage.setNewLanguage(Languages.allCases[row].languageSystemName)
+        default:
+            break
+        }
+        refreshVC()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        30
+    }
+}
+
+// MARK: - UIPickerViewDataSource
+extension ViewController: UIPickerViewDataSource {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        Languages.allCases.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+}
+
